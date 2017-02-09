e6566a371fd72ea61cb8fd19754877da8cd45ac3
Steven Versteeg <sversteeg@mitre.org>
2017-02-09 14:58:13 -0500

    initial version

---
+++ helloworld.java
@@ -0,0 +1,18 @@
+/* 
+ *  they serve to remind us about its properties. The first two lines tell
+ *  us what to type to compile and test the program. The next line describes
+ *  the purpose of the program. The next few lines give a sample execution
+ *  of the program and the resulting output. We will always include such 
+ *  lines in our programs and encourage you to do the same.
+ *
+ ******************************************************************************/
+
+public class helloworld {
+
+    public static void main1(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello World");
+    }
+
+}
+
